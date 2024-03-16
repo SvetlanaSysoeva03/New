45d906800e0ec6aacc9ee077c98b53bbd4f38dd9
sysoeva svetlana <s9192489459@gmail.com>
2024-03-16 15:45:10 +0300

    третья фиксация

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string[] originalArray = { "Hello", "2", "world", ":-)" };
+        string[] newArray = FilterArray(originalArray);
+        
+        Console.WriteLine("Исходный массив:");
+        PrintArray(originalArray);
+        
+        Console.WriteLine("\nНовый массив:");
+        PrintArray(newArray);
+    }
+
+    static string[] FilterArray(string[] array)
+    {
+        int count = 0;
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] result = new string[count];
+        int index = 0;
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                result[index] = str;
+                index++;
+            }
+        }
+
+        return result;
+    }
+
+    static void PrintArray(string[] array)
+    {
+        foreach (string str in array)
+        {
+            Console.Write(str + " ");
+        }
+        Console.WriteLine();
+    }
+}
